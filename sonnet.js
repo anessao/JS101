840265b94e50d52ecf2696116d25994577ff45df
Anessa Ortner <anessa.ortner@gmail.com>
2017-01-24 18:51:38 -0600

    removed console.logs

--- sonnet.js
+++ sonnet.js
@@ -3,22 +3,17 @@
 var element = document.getElementById("sonnet");
 var sonnet = element.innerHTML;
 
-console.log("Original Sonnet: ", sonnet);
 // Find and output the starting position of the word "orphans"
-var orphansIndex = sonnet.indexOf("orphans");
-console.log("Position of 'orphans': ", orphansIndex);
+var orphansIndex = sonnet.indexOf("orphans"); //this prints 402
 
 // Output the number of characters in the sonnet
-var sonnetLength = sonnet.length;
-console.log("Characters in sonnet: ", sonnetLength);
+var sonnetLength = sonnet.length; //this prints 619
 
 // Replace the first occurance of the string "winter" with "yuletide"
 var noWinter = sonnet.replace("winter", "yuletide");
-console.log("Replace Winter with Yuletide: ", noWinter);
 
 // Replace all occurances of the string "the" with "a large"
 var noThe = noWinter.replace(/the\s/g, "a large ");
-console.log("No The: ", noThe);
 
 // Set the content of the sonnet div with the new string
 var newSonnet = noThe;
