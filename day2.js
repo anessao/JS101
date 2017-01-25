884e26575c78e781af45281af71c3ab1b4cb9b7d
Anessa Ortner <anessa.ortner@gmail.com>
2017-01-24 19:21:50 -0600

    completed a switch statement and updated index.html

---
+++ day2.js
@@ -0,0 +1,13 @@
+// switch statement
+var day = "Friday";
+
+switch(day){
+	case "Monday":
+		console.log("Boooooo");
+		break;
+	case "Friday":
+		console.log("PARTAY");
+		break;
+	default: 
+		console.log("not Monday");
+}
