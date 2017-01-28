620c8d5cc1df30d02bbfab7c8e2c5fac7126a0cd
Anessa Ortner <anessa.ortner@gmail.com>
2017-01-27 19:39:12 -0600

    again

--- day2.js
+++ day2.js
@@ -12,43 +12,97 @@
 // 		console.log("not Monday");
 // }
 
-
 //ARRAYS
 
+// var fruit = ["cherries", "apples", "bananas"];
+// var fruitString = fruit.join(", "); //joining the data of an array.  (<-your joining char->)
+// console.log("fruitString", fruitString);
+
+// var catColors = 'green,red,yellow,black';
+// var catColorsArray = catColors.split(",");
+// console.log(catColorsArray);
+
+// var reverseFruit = fruit.reverse();
+// console.log(reverseFruit);
+
+// var alphaFruit = fruit.sort();
+// console.log("alphaFruit", alphaFruit)
+
+// // All this works great until you have an array of #s
+
+// var nums = [9, 6, 8, 2, 4, 11, 66]; //want 2,4,6,8,9,11,66
+// var numSort = nums.sort(function (a,b) {return a-b});
+// console.log("numSort", numSort);
+
+// var animals = ["cats", "turtles", "fish", "bears", "tigers"];
+// console.log(animals.indexOf("fish"));
+
+// var animals2 = animals.slice(2,4); //copies items out of array, doesn't modify original
+// console.log("animals2", animals2);
+
+// animals.splice(1,1);
+
+// var color = ["red","blue","gold","pink","orange"];
+
+// //sort, reverse, make a string
+
+// var finalAnswer = color.sort().reverse().join(" cat ");
+// console.log("final Answer", finalAnswer);
+//(;;) 3 positions. (start val; where you want to stop; counter)
+
+for(var i=0; i<5 ; i++) {
+	console.log(i);
+}
 var fruit = ["cherries", "apples", "bananas"];
-var fruitString = fruit.join(", "); //joining the data of an array.  (<-your joining char->)
-console.log("fruitString", fruitString);
+for(var j=0; j<fruit.length; j++) {
+	console.log("fruit", fruit[j]);
+}
 
-var catColors = 'green,red,yellow,black';
-var catColorsArray = catColors.split(",");
-console.log(catColorsArray);
+//write a for loop that increments by 10 each time = 10 times;
+for (var k=0; k<101; k+=10) {
+	console.log("k",k);
+}
 
-var reverseFruit = fruit.reverse();
-console.log(reverseFruit);
+//for loop that pushes the index to an array for 5 times console.log (array) \\ [0,1,2,3,4]
+var number = [];
+for(var l=0; l<5; l++) {
+ number.unshift(l);
+ console.log(number);
+}
 
-var alphaFruit = fruit.sort();
-console.log("alphaFruit", alphaFruit)
+//for loop that starts at 100 and divides by 2 each time, 3 times = 100,50,25
 
-// All this works great until you have an array of #s
 
-var nums = [9, 6, 8, 2, 4, 11, 66]; //want 2,4,6,8,9,11,66
-var numSort = nums.sort(function (a,b) {return a-b});
-console.log("numSort", numSort);
 
-var animals = ["cats", "turtles", "fish", "bears", "tigers"];
-console.log(animals.indexOf("fish"));
 
-var animals2 = animals.slice(2,4); //copies items out of array, doesn't modify original
-console.log("animals2", animals2);
 
-animals.splice(1,1);
 
-var color = ["red","blue","gold","pink","orange"];
 
-//sort, reverse, make a string
 
-var finalAnswer = color.sort().reverse().join(" cat ");
-console.log("final Answer", finalAnswer);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
